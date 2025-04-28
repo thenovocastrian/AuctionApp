5b20fd26922ea58548747effa7e7fc973d3080a0
Joel C. <thefulldev@proton.me>
2025-04-28 19:05:37 +1000

    Create CRUD Service
    Added a basic CRUD service for maintaining Auctions

---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,22 @@
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        // Mapping Entity to DTO
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item); // Maps Auction linked on Item
+        CreateMap<Item, AuctionDto>();
+        
+        // Mapping DTO back to Entity
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(dest => dest.Item, 
+                opt => opt.MapFrom(src => src)); // Use CreateAuctionDto for both Auction and its Item
+
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}

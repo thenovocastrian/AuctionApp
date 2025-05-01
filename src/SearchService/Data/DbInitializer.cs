24cd08deb13a897a6c9e2670590e1b8a1ceab8d7
Joel C <thefulldev@proton.me>
2025-05-01 20:37:17 +1000

    Refactor database initialization to fetch items via service
    Replaced local file-based seeding with a service-dependent approach by integrating `AuctionSvcHttpClient`. This ensures the search database is populated with up-to-date data directly from the Auction Service. Simplified logic improves maintainability and data consistency.

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
@@ -20,19 +21,15 @@ public class DbInitializer
             .Key(x => x.Color, KeyType.Text)
             .CreateAsync();
         
-        var count = await DB.CountAsync<Item>();
+        using var scope = app.Services.CreateScope();
+        
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
 
-        if (count == 0)
-        {
-            Console.WriteLine("There is no data. Attempting to seed...");
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-            
-            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
-            
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData,options);
-            
-            await DB.SaveAsync(items);
-        }
+        var items = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine(items.Count + " items returned from the Auction Service");
+        
+        if (items.Count != 0) await DB.SaveAsync(items);
 
     }
 }
